98cf6424f4e2fada04261b43516efe3c73c49528
Nicolò Ribaudo <hello@nicr.dev>
2023-08-08 21:42:53 +0200

    Support prefix filter in get-release-tags

--- get-release-tags/main.js
+++ get-release-tags/main.js
@@ -3,10 +3,12 @@ cp.execSync(`cd ${__dirname}; npm ci`);
 
 const core = require("@actions/core");
 
+const prefix = core.getInput("token") ?? "";
+
 const exec = cmd => cp.execSync(cmd).toString().trim();
 
-const currentTag = exec(`git describe --abbrev=0 --tags HEAD`);
-const lastTag = exec(`git describe --abbrev=0 --tags ${currentTag}^`);
+const currentTag = exec(`git describe --abbrev=0 --tags --match '${prefix}*' HEAD`);
+const lastTag = exec(`git describe --abbrev=0 --tags '${prefix}*' ${currentTag}^`);
 
 core.setOutput("old", lastTag);
 core.setOutput("new", currentTag);
